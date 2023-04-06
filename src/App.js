242e241710bfc455633b9f167ee91c78d4b59da5
MiraVyslovukh <mira@talkable.com>
2023-04-06 22:55:28 +0300

    fixed noreferrer error

--- src/App.js
+++ src/App.js
@@ -6,21 +6,24 @@ export default function App() {
   return (
     <div className="App">
       <Engine />
-      <div className="row justify-content-center by">
-        <footer id="by">
-          This project was coded by Mira Vyslovukh and is open-sourced on{" "}
-          <a
-            href="https://github.com/MiraVyslovukh/weather-app-react-homework"
-            target="_blank"
-          >
-            Github
-          </a>{" "}
-          and hosted on{" "}
-          <a href="https://zingy-chimera-4365ca.netlify.app" target="_blank">
-            Netlify
-          </a>
-        </footer>
-      </div>
+      <footer id="by">
+        This project was coded by Mira Vyslovukh and is open-sourced on{" "}
+        <a
+          href="https://github.com/MiraVyslovukh/weather-app-react-homework"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Github
+        </a>{" "}
+        and hosted on{" "}
+        <a
+          href="https://zingy-chimera-4365ca.netlify.app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Netlify
+        </a>
+      </footer>
     </div>
   );
 }
