0db7506285fc3d333abb4dc59e5f964bde7e9801
MiraVyslovukh <mira@talkable.com>
2023-04-07 12:39:57 +0300

    weatherData props deconstructed

--- src/Weather.js
+++ src/Weather.js
@@ -2,17 +2,19 @@ import React from "react";
 import "./Weather.css";
 
 export default function Weather(props) {
-  if (props.loaded)
+  const { loaded, newCity, description, icon, temperature, humidity, wind } =
+    props.weatherData;
+  if (loaded) {
     return (
       <div className="Weather">
         <div className="container">
-          <h3>{props.newCity}</h3>
+          <h3>{newCity}</h3>
           <h5>Friday, 13:02</h5>
-          <h5 class="text-capitalize font-weight-bold">{props.description}</h5>
+          <h5 class="text-capitalize font-weight-bold">{description}</h5>
           <div className="row">
             <div className="col-2 mt-2">
               <img
-                src={`http://openweathermap.org/img/wn/${props.icon}.png`}
+                src={`http://openweathermap.org/img/wn/${icon}.png`}
                 style={{ display: "block", width: 70, margin: "0 auto" }}
                 alt="{props.description}"
               />
@@ -20,17 +22,17 @@ export default function Weather(props) {
 
             <div className="col-4 mt-2 mb-3">
               <div className="temperature">
-                <span class="temperature">{Math.round(props.temperature)}</span>
+                <span class="temperature">{Math.round(temperature)}</span>
                 <span class="unit">°C</span>
               </div>
             </div>
             <div className="col-6 mt-4 mb-3">
               <ul className="message">
                 <li>
-                  <strong>Humidity:</strong> {props.humidity}%
+                  <strong>Humidity:</strong> {humidity}%
                 </li>
                 <li>
-                  <strong>Wind:</strong> {props.wind}km/h
+                  <strong>Wind:</strong> {wind}km/h
                 </li>
               </ul>
             </div>
@@ -38,4 +40,5 @@ export default function Weather(props) {
         </div>
       </div>
     );
+  }
 }
